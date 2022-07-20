4d7b8e9940e22aa54a80e1fbcb6812973bfd928b
SFSADespair <tyrell.snyders@gmail.com>
2022-07-20 09:57:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+        }
+    }
+}
