b379b3c4f7f0f1cacae1cc01b39d153ef08a3bdf
SFSADespair <tyrell.snyders@gmail.com>
2022-07-20 11:32:00 +0200

    Added guessing function in a nested while loop. Displays the guess after guessing a number.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,27 @@ namespace GuessTheNumber
         static void Main(string[] args)
         {
             Random random = new Random();
+            int number;
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess = 0;
+            int guesses = 0;
 
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                number = random.Next(min, max + 1);
+                while (guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + ": ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+                }
+            }
+            
+            Console.ReadKey();
         }
     }
 }
